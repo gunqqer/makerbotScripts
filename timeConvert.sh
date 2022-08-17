f86e1921d5f1a420905011cb1ce2cc6070436386
gunqqer friithian <gunqqer@friithian.dev>
2022-08-17 14:50:48 -0400

    Add comments to conversion script

--- timeConvert.sh
+++ timeConvert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#Take makerbot's mildly annoying time format and turns it into something SQLite's datetime() function can parse. This script only does this for the Time Submitted field and the Time Complete may be N/A and it really isn't very useful
+#Usage: ./timeConvert.sh < ./file.csv > ./outFile.csv
 read -r; printf "$REPLY\n"
 while IFS=$',' read -r c0 c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11; do
 	printf "$c0,$c1,$(date --utc +%Y-%m-%dT%H:%M:%S -d "$c2"),$c3,$c4,$c5,$c6,$c7,$c8,$c9,$c10,$c11\n"
