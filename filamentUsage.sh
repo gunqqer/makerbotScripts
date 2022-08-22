fed4c9fb3c09fe7ac0ed70e599a34a0c6973912e
gunqqer friithian <gunqqer@friithian.dev>
2022-08-22 10:23:25 -0400

    Remove unneeded newlines

--- filamentUsage.sh
+++ filamentUsage.sh
@@ -2,12 +2,12 @@
 #Gives total usage of filament by user and only looks at prints that completed
 if [[ -z "$1" ]];
 then
-	printf "\n$(tput setab 196)File name of the CSV is missing!$(tput sgr0)\n"
+	printf "$(tput setab 196)File name of the CSV is missing!$(tput sgr0)\n"
 	exit 1
 fi
 if [[ $1 = *" "* ]];
 then
-	printf "\n$(tput setab 196)File name of the CSV contains a space! Please rename the file and try again$(tput sgr0)\n"
+	printf "$(tput setab 196)File name of the CSV contains a space! Please rename the file and try again$(tput sgr0)\n"
 	exit 1
 fi
 
--- printCount.sh
+++ printCount.sh
@@ -2,12 +2,12 @@
 #Gives numbers of prints per user, including failed and cancelled 
 if [[ -z "$1" ]];
 then
-	printf "\n$(tput setab 196)File name of the CSV is missing!$(tput sgr0)\n"
+	printf "$(tput setab 196)File name of the CSV is missing!$(tput sgr0)\n"
 	exit 1
 fi
 if [[ $1 = *" "* ]];
 then
-	printf "\n$(tput setab 196)File name of the CSV contains a space! Please rename the file and try again$(tput sgr0)\n"
+	printf "$(tput setab 196)File name of the CSV contains a space! Please rename the file and try again$(tput sgr0)\n"
 	exit 1
 fi
 
