ad837a5ce62f35ad048367139bcbefc4c527d216
gunqqer friithian <gunqqer@friithian.dev>
2022-11-24 15:37:56 -0500

    add new script for total overall usage

--- filamentUsage.sh
+++ filamentUsage.sh
@@ -10,5 +10,4 @@ then
 	printf "$(tput setab 196)File name of the CSV contains a space! Please rename the file and try again$(tput sgr0)\n"
 	exit 1
 fi
-
-q -H -d, "SELECT \"Submitted By\", SUM(\"Total Estimated Material Usage\")  FROM $1 WHERE \"Print Job Status\" = 'completed' GROUP BY \"Submitted By\" ORDER BY \"Total Estimated Material Usage\""
+q -H -d, "SELECT \"Submitted By\", SUM(\"Total Estimated Material Usage\")  FROM $1 WHERE \"Print Job Status\" = 'completed' GROUP BY \"Submitted By\" ORDER BY \"Total Estimated Material Usage\" DESC"
